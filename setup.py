4ee48ed3e7403c00c5b9f3cf864fcb7c113d6946
Wolf <wolf@tgwolf.com>
2024-06-26 17:27:46 +0100

    split requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", 'r', encoding='UTF-8') as f:
 
 setup(
     name='wolfsoftware.NQueens',
-    version='0.1.0',
+    version='0.1.1',
     packages=['wolfsoftware.nqueens'],
     entry_points={
         'console_scripts': [
